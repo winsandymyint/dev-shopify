7da7a9965512f6a5cf65f1c1fb0d44ec27283af5
winsandymyint <winsandymyint@amdon.com>
2015-11-02 16:18:45 +0630

    Debug Shopify API

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ else {
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser({ type: 'application/*+json' }));
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: "shhhhh!!!!" }));
@@ -132,7 +132,7 @@ function parseRequestBody(req, res) {
         req.body += chunk.toString('utf8');
     });
     console.log("Debug ----------------------2")
-    console.log(req.body)
+    console.log(req)
     console.log("Debug ----------------------2.0.1")
 
     req.on('end', function() {
@@ -160,20 +160,17 @@ function handleRequest(req, res) {
     }
 }
 
-function bookSubscribe (req) {
-	console.log(req.body)
-	testData= {"id":123456,"email":"jon@doe.ca","closed_at":null,"created_at":"2015-10-29T07:46:42-04:00","updated_at":"2015-10-29T07:46:42-04:00","number":234,"note":null,"token":null,"gateway":null,"test":true,"total_price":"234.94","subtotal_price":"224.94","total_weight":0,"total_tax":"0.00","taxes_included":false,"currency":"USD","financial_status":"voided","confirmed":false,"total_discounts":"5.00","total_line_items_price":"229.94","cart_token":null,"buyer_accepts_marketing":true,"name":"#9999","referring_site":null,"landing_site":null,"cancelled_at":"2015-10-29T07:46:42-04:00","cancel_reason":"customer","total_price_usd":null,"checkout_token":null,"reference":null,"user_id":null,"location_id":null,"source_identifier":null,"source_url":null,"processed_at":null,"device_id":null,"browser_ip":null,"landing_site_ref":null,"order_number":1234,"discount_codes":[],"note_attributes":[],"payment_gateway_names":["bogus"],"processing_method":"","checkout_id":null,"source_name":"web","fulfillment_status":"pending","tax_lines":[],"tags":"","contact_email":"jon@doe.ca","line_items":[{"id":56789,"variant_id":null,"title":"Sledgehammer","quantity":1,"price":"199.99","grams":5000,"sku":"SKU2006-001","variant_title":null,"vendor":null,"fulfillment_service":"manual","product_id":327475578523353102,"requires_shipping":true,"taxable":true,"gift_card":false,"name":"Sledgehammer","variant_inventory_management":null,"properties":[],"product_exists":true,"fulfillable_quantity":1,"total_discount":"0.00","fulfillment_status":null,"tax_lines":[]},{"id":98765,"variant_id":null,"title":"Wire Cutter","quantity":1,"price":"29.95","grams":500,"sku":"SKU2006-020","variant_title":null,"vendor":null,"fulfillment_service":"manual","product_id":327475578523353102,"requires_shipping":true,"taxable":true,"gift_card":false,"name":"Wire Cutter","variant_inventory_management":null,"properties":[],"product_exists":true,"fulfillable_quantity":1,"total_discount":"5.00","fulfillment_status":null,"tax_lines":[]}],"shipping_lines":[{"title":"Generic Shipping","price":"10.00","code":null,"source":"shopify","phone":null,"tax_lines":[]}],"billing_address":{"first_name":"Bob","address1":"123 Billing Street","phone":"555-555-BILL","city":"Billtown","zip":"K2P0B0","province":"Kentucky","country":"United States","last_name":"Biller","address2":null,"company":"My Company","latitude":null,"longitude":null,"name":"Bob Biller","country_code":"US","province_code":"KY"},"shipping_address":{"first_name":"Steve","address1":"123 Shipping Street","phone":"555-555-SHIP","city":"Shippington","zip":"K2P0S0","province":"Kentucky","country":"United States","last_name":"Shipper","address2":null,"company":"Shipping Company","latitude":null,"longitude":null,"name":"Steve Shipper","country_code":"US","province_code":"KY"},"fulfillments":[],"refunds":[],"customer":{"id":null,"email":"john@test.com","accepts_marketing":false,"created_at":null,"updated_at":null,"first_name":"John","last_name":"Smith","orders_count":0,"state":"disabled","total_spent":"0.00","last_order_id":null,"note":null,"verified_email":true,"multipass_identifier":null,"tax_exempt":false,"tags":"","last_order_name":null,"default_address":{"id":null,"first_name":null,"last_name":null,"company":null,"address1":"123 Elm St.","address2":null,"city":"Ottawa","province":"Ontario","country":"Canada","zip":"K2H7A8","phone":"123-123-1234","name":"","province_code":"ON","country_code":"CA","country_name":"Canada","default":true}}}
-
+function bookSubscribe (req, res) {
+	var obj = JSON.parse(req);
 	console.log(testData.customer.email)
 	console.log(testData.customer.first_name)
-	console.log(testData.fulfillment_status=='pending')
-	if(testData.fulfillment_status=='pending'){ // Check the payment is successfully or not! Fulfilled 
-		//not sure line_items.fulfillment_status == ?
+	console.log(testData.fulfillment_status=='fulfilled')
+	if(testData.fulfillment_status=='fulfilled'){ 
 		arr= []
-		for(i in testData.line_items) { testData.line_items[i].sku= '55d6d6018ce4b15b956492e5'; arr.push(testData.line_items[i].sku) }
+		for(i in obj.line_items) { arr.push(obj.line_items[i].sku) }
 	    data= {
-		  "Email": 'winsandymyint@gmail.com', //testData.customer.email,
-		  "Displayname": testData.customer.first_name,
+		  "Email": obj.email,
+		  "Displayname": obj.customer.first_name,
 		  "BookID": arr
 		}
 		console.log(data)
