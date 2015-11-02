2904f1a663913d9d8235f6d9ecaa0077169d5531
winsandymyint <winsandymyint@amdon.com>
2015-11-02 16:34:18 +0630

    Change Procfile

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const PORT = 3000;
-var config = require ('./config.json');
-const SECRET = 'c3a36925eb2ce2244aaa436742df3607';
+var config = require ('./config.json')
+  , request = require('request')
+var url= 'http://requestb.in/z2in7fz2'
+const SECRET = '80a2b388673a5e8f562e189689a795c0';
 
 var http = require('http'),
     crypto = require('crypto'),
     server;
 
 function verifyShopifyHook(req) {
-    var digest = crypto.createHmac('SHA256', SECRET)
+    var digest = crypto.createHmac('SHA256', '3cdb276557ce076221a416efa6270ab1d97c34ae4f0757c4e75b5dc0cf95e4f0')
             .update(new Buffer(req.body, 'utf8'))
             .digest('base64');
-    
-    console.log("###########")
-    console.log(req.headers['X-Shopify-Hmac-Sha256'])
-    console.log(digest)
-    console.log("!---------!")
-    return digest === req.headers['X-Shopify-Hmac-Sha256'];
+            console.log("$$$$$$")
+            console.log(digest)
+            console.log(req.headers['x-shopify-hmac-sha256'])
+            console.log("%%%%%%")
+    return digest === req.headers['x-shopify-hmac-sha256'];
 }
 
 function parseRequestBody(req, res) {
