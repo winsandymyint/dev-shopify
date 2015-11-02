8f10bafd2ae27a74e35a129fd82d22806a155a1e
winsandymyint <winsandymyint@amdon.com>
2015-11-02 18:12:33 +0630

    APP

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ else {
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({ type: 'application/*+json' }))
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: "shhhhh!!!!" }));
